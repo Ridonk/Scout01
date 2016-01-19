6299a44b468765991be0d5e524f5ae659264a667
Ridonk <georgeddunbar@gmail.com>
2016-01-19 15:55:44 -0500

    Update package structure

---
+++ calc/unitDB.java
@@ -0,0 +1,377 @@
+package calc;
+
+public class unitDB {
+	
+	// Create Infantry Units
+	public unit Militia;
+	public unit Infantry;
+	public unit Spearman;
+	public unit Swordsman;
+	public unit Pikeman;
+	public unit NobleSwordsman;
+	public unit Guard;
+	public unit HeavyPikeman;
+	public unit Halberdier;
+	public unit Berserker;
+	
+	// Create archer units
+	public unit Shortbowman;
+	public unit Longbowman;
+	public unit Crossbowman;
+	public unit Arbalester;
+	public unit EliteLongbowman;
+	public unit ArcherGuard;
+	public unit HeavyCrossbowman;
+	public unit EagleArcher;
+	public unit Windlassman;
+	public unit Marksman;
+	
+	// Create cavalry units
+	public unit Rider;
+	public unit LightCavalry;
+	public unit HeavyCavalry;
+	public unit MountedArcher;
+	public unit CavalryShooter;
+	public unit KnightsTemplar;
+	public unit HeavyCArcher;
+	public unit RoyalKnight;
+	public unit StrikeArcher;
+	public unit DivineKnight;
+	
+	// Create siege units
+	public unit Bricole;
+	public unit AssaultCart;
+	public unit Mangonel;
+	public unit BatteringRam;
+	public unit HeavyMangonel;
+	public unit Demolisher;
+	public unit Trebuchet;
+	public unit HeavyTrebuchet;
+	public unit SiegeTower;
+	public unit MightyTrebuchet;
+	
+	public unitDB() {
+		
+	}
+
+	public unit getMilitia() {
+		return Militia;
+	}
+
+	public void setMilitia(unit militia) {
+		Militia = militia;
+	}
+
+	public unit getInfantry() {
+		return Infantry;
+	}
+
+	public void setInfantry(unit infantry) {
+		Infantry = infantry;
+	}
+
+	public unit getSpearman() {
+		return Spearman;
+	}
+
+	public void setSpearman(unit spearman) {
+		Spearman = spearman;
+	}
+
+	public unit getSwordsman() {
+		return Swordsman;
+	}
+
+	public void setSwordsman(unit swordsman) {
+		Swordsman = swordsman;
+	}
+
+	public unit getPikeman() {
+		return Pikeman;
+	}
+
+	public void setPikeman(unit pikeman) {
+		Pikeman = pikeman;
+	}
+
+	public unit getNobleSwordsman() {
+		return NobleSwordsman;
+	}
+
+	public void setNobleSwordsman(unit nobleSwordsman) {
+		NobleSwordsman = nobleSwordsman;
+	}
+
+	public unit getGuard() {
+		return Guard;
+	}
+
+	public void setGuard(unit guard) {
+		Guard = guard;
+	}
+
+	public unit getHeavyPikeman() {
+		return HeavyPikeman;
+	}
+
+	public void setHeavyPikeman(unit heavyPikeman) {
+		HeavyPikeman = heavyPikeman;
+	}
+
+	public unit getHalberdier() {
+		return Halberdier;
+	}
+
+	public void setHalberdier(unit halberdier) {
+		Halberdier = halberdier;
+	}
+
+	public unit getBerserker() {
+		return Berserker;
+	}
+
+	public void setBerserker(unit berserker) {
+		Berserker = berserker;
+	}
+
+	public unit getShortbowman() {
+		return Shortbowman;
+	}
+
+	public void setShortbowman(unit shortbowman) {
+		Shortbowman = shortbowman;
+	}
+
+	public unit getLongbowman() {
+		return Longbowman;
+	}
+
+	public void setLongbowman(unit longbowman) {
+		Longbowman = longbowman;
+	}
+
+	public unit getCrossbowman() {
+		return Crossbowman;
+	}
+
+	public void setCrossbowman(unit crossbowman) {
+		Crossbowman = crossbowman;
+	}
+
+	public unit getArbalester() {
+		return Arbalester;
+	}
+
+	public void setArbalester(unit arbalester) {
+		Arbalester = arbalester;
+	}
+
+	public unit getEliteLongbowman() {
+		return EliteLongbowman;
+	}
+
+	public void setEliteLongbowman(unit eliteLongbowman) {
+		EliteLongbowman = eliteLongbowman;
+	}
+
+	public unit getArcherGuard() {
+		return ArcherGuard;
+	}
+
+	public void setArcherGuard(unit archerGuard) {
+		ArcherGuard = archerGuard;
+	}
+
+	public unit getHeavyCrossbowman() {
+		return HeavyCrossbowman;
+	}
+
+	public void setHeavyCrossbowman(unit heavyCrossbowman) {
+		HeavyCrossbowman = heavyCrossbowman;
+	}
+
+	public unit getEagleArcher() {
+		return EagleArcher;
+	}
+
+	public void setEagleArcher(unit eagleArcher) {
+		EagleArcher = eagleArcher;
+	}
+
+	public unit getWindlassman() {
+		return Windlassman;
+	}
+
+	public void setWindlassman(unit windlassman) {
+		Windlassman = windlassman;
+	}
+
+	public unit getMarksman() {
+		return Marksman;
+	}
+
+	public void setMarksman(unit marksman) {
+		Marksman = marksman;
+	}
+
+	public unit getRider() {
+		return Rider;
+	}
+
+	public void setRider(unit rider) {
+		Rider = rider;
+	}
+
+	public unit getLightCavalry() {
+		return LightCavalry;
+	}
+
+	public void setLightCavalry(unit lightCavalry) {
+		LightCavalry = lightCavalry;
+	}
+
+	public unit getHeavyCavalry() {
+		return HeavyCavalry;
+	}
+
+	public void setHeavyCavalry(unit heavyCavalry) {
+		HeavyCavalry = heavyCavalry;
+	}
+
+	public unit getMountedArcher() {
+		return MountedArcher;
+	}
+
+	public void setMountedArcher(unit mountedArcher) {
+		MountedArcher = mountedArcher;
+	}
+
+	public unit getCavalryShooter() {
+		return CavalryShooter;
+	}
+
+	public void setCavalryShooter(unit cavalryShooter) {
+		CavalryShooter = cavalryShooter;
+	}
+
+	public unit getKnightsTemplar() {
+		return KnightsTemplar;
+	}
+
+	public void setKnightsTemplar(unit knightsTemplar) {
+		KnightsTemplar = knightsTemplar;
+	}
+
+	public unit getHeavyCArcher() {
+		return HeavyCArcher;
+	}
+
+	public void setHeavyCArcher(unit heavyCArcher) {
+		HeavyCArcher = heavyCArcher;
+	}
+
+	public unit getRoyalKnight() {
+		return RoyalKnight;
+	}
+
+	public void setRoyalKnight(unit royalKnight) {
+		RoyalKnight = royalKnight;
+	}
+
+	public unit getStrikeArcher() {
+		return StrikeArcher;
+	}
+
+	public void setStrikeArcher(unit strikeArcher) {
+		StrikeArcher = strikeArcher;
+	}
+
+	public unit getDivineKnight() {
+		return DivineKnight;
+	}
+
+	public void setDivineKnight(unit divineKnight) {
+		DivineKnight = divineKnight;
+	}
+
+	public unit getBricole() {
+		return Bricole;
+	}
+
+	public void setBricole(unit bricole) {
+		Bricole = bricole;
+	}
+
+	public unit getAssaultCart() {
+		return AssaultCart;
+	}
+
+	public void setAssaultCart(unit assaultCart) {
+		AssaultCart = assaultCart;
+	}
+
+	public unit getMangonel() {
+		return Mangonel;
+	}
+
+	public void setMangonel(unit mangonel) {
+		Mangonel = mangonel;
+	}
+
+	public unit getBatteringRam() {
+		return BatteringRam;
+	}
+
+	public void setBatteringRam(unit batteringRam) {
+		BatteringRam = batteringRam;
+	}
+
+	public unit getHeavyMangonel() {
+		return HeavyMangonel;
+	}
+
+	public void setHeavyMangonel(unit heavyMangonel) {
+		HeavyMangonel = heavyMangonel;
+	}
+
+	public unit getDemolisher() {
+		return Demolisher;
+	}
+
+	public void setDemolisher(unit demolisher) {
+		Demolisher = demolisher;
+	}
+
+	public unit getTrebuchet() {
+		return Trebuchet;
+	}
+
+	public void setTrebuchet(unit trebuchet) {
+		Trebuchet = trebuchet;
+	}
+
+	public unit getHeavyTrebuchet() {
+		return HeavyTrebuchet;
+	}
+
+	public void setHeavyTrebuchet(unit heavyTrebuchet) {
+		HeavyTrebuchet = heavyTrebuchet;
+	}
+
+	public unit getSiegeTower() {
+		return SiegeTower;
+	}
+
+	public void setSiegeTower(unit siegeTower) {
+		SiegeTower = siegeTower;
+	}
+
+	public unit getMightyTrebuchet() {
+		return MightyTrebuchet;
+	}
+
+	public void setMightyTrebuchet(unit mightyTrebuchet) {
+		MightyTrebuchet = mightyTrebuchet;
+	}
+	
+}
