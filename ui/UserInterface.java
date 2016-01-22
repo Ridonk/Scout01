d402167cf1648d36a35c6971443a51b2f0388c8b
Ridonk <georgeddunbar@gmail.com>
2016-01-22 06:29:14 -0500

    Updated GUI scheme to include variable colors and resolved some errors.

--- ui/UserInterface.java
+++ ui/UserInterface.java
@@ -54,57 +54,71 @@ public class UserInterface {
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {		
+	private void initialize() {
 		frmScout = new JFrame();
+		Color background = new Color(255, 255, 255);
+		Color foreground = new Color(30, 144, 255);
+		Color backgroundTextField = new Color(204,255,255);
+		frmScout.setForeground(foreground);
+		frmScout.getContentPane().setForeground(foreground);
+		frmScout.setBackground(background);
 		frmScout.setResizable(false);
 		frmScout.setIconImage(Toolkit.getDefaultToolkit().getImage(UserInterface.class.getResource("/ui/nte.gif")));
 		frmScout.setTitle("Scout");
 		frmScout.getContentPane().setBackground(Color.WHITE);
-		frmScout.setBounds(100, 100, 315, 357);
+		frmScout.setBounds(100, 100, 360, 370);
 		frmScout.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		SpringLayout springLayout = new SpringLayout();
 		frmScout.getContentPane().setLayout(springLayout);
 		
 		lordPower = new JTextField();
+		lordPower.setForeground(foreground);
 		springLayout.putConstraint(SpringLayout.NORTH, lordPower, 15, SpringLayout.NORTH, frmScout.getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST, lordPower, 179, SpringLayout.WEST, frmScout.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, lordPower, 190, SpringLayout.WEST, frmScout.getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, lordPower, -15, SpringLayout.EAST, frmScout.getContentPane());
 		lordPower.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lordPower.setBackground(Color.LIGHT_GRAY);
+		lordPower.setBackground(backgroundTextField);
 		frmScout.getContentPane().add(lordPower);
 		lordPower.setColumns(10);
 		
 		lordCastleLevel = new JTextField();
+		lordCastleLevel.setForeground(foreground);
 		springLayout.putConstraint(SpringLayout.NORTH, lordCastleLevel, 5, SpringLayout.SOUTH, lordPower);
-		springLayout.putConstraint(SpringLayout.WEST, lordCastleLevel, 179, SpringLayout.WEST, frmScout.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, lordCastleLevel, 190, SpringLayout.WEST, frmScout.getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, lordCastleLevel, -15, SpringLayout.EAST, frmScout.getContentPane());
 		lordCastleLevel.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lordCastleLevel.setBackground(Color.LIGHT_GRAY);
+		lordCastleLevel.setBackground(backgroundTextField);
 		lordCastleLevel.setColumns(10);
 		frmScout.getContentPane().add(lordCastleLevel);
 		
 		JLabel labelLordPower = new JLabel("Enter the Lord's Power");
+		labelLordPower.setFont(new Font("Tahoma", Font.BOLD, 11));
+		labelLordPower.setForeground(foreground);
 		springLayout.putConstraint(SpringLayout.NORTH, labelLordPower, 3, SpringLayout.NORTH, lordPower);
 		springLayout.putConstraint(SpringLayout.WEST, labelLordPower, 15, SpringLayout.WEST, frmScout.getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, labelLordPower, -15, SpringLayout.WEST, lordPower);
 		frmScout.getContentPane().add(labelLordPower);
 		
 		JLabel labelLordCastleLevel = new JLabel("Enter the Lord's Castle Level");
+		labelLordCastleLevel.setFont(new Font("Tahoma", Font.BOLD, 11));
+		labelLordCastleLevel.setForeground(foreground);
 		springLayout.putConstraint(SpringLayout.NORTH, labelLordCastleLevel, 3, SpringLayout.NORTH, lordCastleLevel);
 		springLayout.putConstraint(SpringLayout.WEST, labelLordCastleLevel, 15, SpringLayout.WEST, frmScout.getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, labelLordCastleLevel, -15, SpringLayout.WEST, lordCastleLevel);
 		frmScout.getContentPane().add(labelLordCastleLevel);
 		
 		JTextPane lordTroopBreakdown = new JTextPane();
+		lordTroopBreakdown.setForeground(foreground);
 		springLayout.putConstraint(SpringLayout.WEST, lordTroopBreakdown, 15, SpringLayout.WEST, frmScout.getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, lordTroopBreakdown, -15, SpringLayout.SOUTH, frmScout.getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, lordTroopBreakdown, -15, SpringLayout.EAST, frmScout.getContentPane());
 		frmScout.getContentPane().add(lordTroopBreakdown);
 		
 		JButton btnCalculate = new JButton("Calculate");
+		btnCalculate.setForeground(foreground);
+		springLayout.putConstraint(SpringLayout.WEST, btnCalculate, 190, SpringLayout.WEST, frmScout.getContentPane());
 		springLayout.putConstraint(SpringLayout.NORTH, lordTroopBreakdown, 15, SpringLayout.SOUTH, btnCalculate);
 		springLayout.putConstraint(SpringLayout.NORTH, btnCalculate, 5, SpringLayout.SOUTH, lordCastleLevel);
-		springLayout.putConstraint(SpringLayout.WEST, btnCalculate, 179, SpringLayout.WEST, frmScout.getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, btnCalculate, -15, SpringLayout.EAST, frmScout.getContentPane());
 		btnCalculate.setFont(new Font("Tahoma", Font.BOLD, 12));
 		btnCalculate.addActionListener(new ActionListener() {
@@ -116,12 +130,18 @@ public class UserInterface {
 		frmScout.getContentPane().add(btnCalculate);
 		
 		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBorderPainted(false);
+		menuBar.setBackground(background);
 		frmScout.setJMenuBar(menuBar);
 		
 		JMenu mnFile = new JMenu("File");
+		mnFile.setBackground(background);
+		mnFile.setForeground(foreground);
 		menuBar.add(mnFile);
 		
 		JMenuItem mntmClose = new JMenuItem("Close");
+		mntmClose.setBackground(background);
+		mntmClose.setForeground(foreground);
 		mntmClose.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
@@ -130,9 +150,13 @@ public class UserInterface {
 		mnFile.add(mntmClose);
 		
 		JMenu mnHelp = new JMenu("Help");
+		mnHelp.setBackground(background);
+		mnHelp.setForeground(foreground);
 		menuBar.add(mnHelp);
 		
 		JMenuItem mntmHowToUse = new JMenuItem("How to use");
+		mntmHowToUse.setBackground(background);
+		mntmHowToUse.setForeground(foreground);
 		mntmHowToUse.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JOptionPane.showMessageDialog(frmScout, "Simply put the lord power and castle level of your target in and click"
@@ -142,6 +166,8 @@ public class UserInterface {
 		mnHelp.add(mntmHowToUse);
 		
 		JMenuItem mntmAbout = new JMenuItem("About");
+		mntmAbout.setBackground(background);
+		mntmAbout.setForeground(foreground);
 		mntmAbout.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JOptionPane.showMessageDialog(frmScout, ""
@@ -158,7 +184,7 @@ public class UserInterface {
 	private void initUnits(Scout masterScout, JTextPane lordTroopBreakdown) {
 		
 		unitDB unitDB = new unitDB();
-		
+				
 		// Create infantry units
 		unitDB.setMilitia(new unit(1, 1, 6, 14, 8, 8, 10.46, 1, 0.21, 1.0));
 		unitDB.Militia.setName("Militia");
@@ -171,11 +197,11 @@ public class UserInterface {
 		unitDB.setPikeman(new unit(5, 13, 38, 22, 15, 9, 13.07, 1, 1.04, 3.2));
 		unitDB.Pikeman.setName("Pikeman");
 		unitDB.setNobleSwordsman(new unit(6, 16, 24, 56, 22, 8, 13.07, 1, 1.25, 4.0));
-		unitDB.NobleSwordsman.setName("NobleSwordsman");
+		unitDB.NobleSwordsman.setName("Noble Swordsman");
 		unitDB.setGuard(new unit(7, 19, 29, 68, 26, 8, 14.07, 1, 1.46, 4.9));
 		unitDB.Guard.setName("Guard");
 		unitDB.setHeavyPikeman(new unit(8, 22, 70, 41, 15, 9, 14.38, 1, 1.67, 5.9));
-		unitDB.HeavyPikeman.setName("HeavyPikeman");
+		unitDB.HeavyPikeman.setName("Heavy Pikeman");
 		unitDB.setHalberdier(new unit(9, 26, 84, 49, 18, 9, 15.68, 1, 1.88, 7.0));
 		unitDB.Halberdier.setName("Halberdier");
 		unitDB.setBerserker(new unit(10, 30, 49, 114, 42, 8, 15.68, 1, 2.08, 8.2));
@@ -191,13 +217,13 @@ public class UserInterface {
 		unitDB.setArbalester(new unit(4, 10, 35, 17, 5, 8, 10.46, 8, 0.83, 2.5));
 		unitDB.Arbalester.setName("Arbalester");
 		unitDB.setEliteLongbowman(new unit(5, 13, 25, 19, 6, 8, 13.07, 5, 1.04, 3.2));
-		unitDB.EliteLongbowman.setName("EliteLongbowman");
+		unitDB.EliteLongbowman.setName("Elite Longbowman");
 		unitDB.setArcherGuard(new unit(6, 16, 32, 24, 8, 8, 13.07, 5, 1.25, 4));
-		unitDB.ArcherGuard.setName("ArcherGuard");
+		unitDB.ArcherGuard.setName("Archer Guard");
 		unitDB.setHeavyCrossbowman(new unit(7, 19, 68, 34, 10, 8, 13.07, 8, 1.46, 4.9));
-		unitDB.HeavyCrossbowman.setName("HeavyCrossbowman");
+		unitDB.HeavyCrossbowman.setName("Heavy Crossbowman");
 		unitDB.setEagleArcher(new unit(8, 22, 47, 35, 11, 8, 14.38, 5, 1.67, 5.9));
-		unitDB.EagleArcher.setName("EagleArcher");
+		unitDB.EagleArcher.setName("Eagle Archer");
 		unitDB.setWindlassman(new unit(9, 26, 98, 49, 13, 8, 14.38, 8, 1.88, 7.0));
 		unitDB.Windlassman.setName("Windlassman");
 		unitDB.setMarksman(new unit(10, 30, 65, 49, 15, 8, 15.68, 5, 2.08, 9.2));
@@ -207,45 +233,45 @@ public class UserInterface {
 		unitDB.setRider(new unit(1, 1, 11, 8, 4, 14, 7.84, 1, 0.21, 1.0));
 		unitDB.Rider.setName("Rider");
 		unitDB.setLightCavalry(new unit(2, 4, 15, 11, 4, 14, 7.84, 1, 0.42, 1.4));
-		unitDB.LightCavalry.setName("LightCavalry");
+		unitDB.LightCavalry.setName("Light Cavalry");
 		unitDB.setHeavyCavalry(new unit(3, 7, 20, 15, 6, 14, 9.15, 1, 0.63, 1.9));
-		unitDB.HeavyCavalry.setName("HeavyCavalry");
+		unitDB.HeavyCavalry.setName("Heavy Cavalry");
 		unitDB.setMountedArcher(new unit(4, 10, 32, 17, 7, 13, 9.15, 4, 0.83, 2.5));
-		unitDB.MountedArcher.setName("MountedArcher");
+		unitDB.MountedArcher.setName("Mounted Archer");
 		unitDB.setCavalryShooter(new unit(5, 13, 41, 22, 9, 13, 10.46, 4, 1.04, 3.2));
-		unitDB.CavalryShooter.setName("CavalryShooter");
+		unitDB.CavalryShooter.setName("Cavalry Shooter");
 		unitDB.setKnightsTemplar(new unit(6, 16, 44, 22, 9, 14, 10.46, 1, 1.25, 4.0));
-		unitDB.KnightsTemplar.setName("KnightsTemplar");
+		unitDB.KnightsTemplar.setName("Knights Templar");
 		unitDB.setHeavyCArcher(new unit(7, 19, 63, 34, 13, 13, 11.76, 4, 1.46, 4.9));
-		unitDB.HeavyCArcher.setName("HeavyCArcher");
+		unitDB.HeavyCArcher.setName("Heavy Calvary Archer");
 		unitDB.setRoyalKnight(new unit(8, 22, 64, 47, 15, 14, 11.76, 1, 1.67, 5.9));
-		unitDB.RoyalKnight.setName("RoyalKnight");
+		unitDB.RoyalKnight.setName("Royal Knight");
 		unitDB.setStrikeArcher(new unit(9, 26, 91, 49, 18, 13, 13.07, 4, 1.88, 7.0));
-		unitDB.StrikeArcher.setName("StrikeArcher");
+		unitDB.StrikeArcher.setName("Strike Archer");
 		unitDB.setDivineKnight(new unit(10, 30, 90, 65, 21, 14, 13.07, 1, 2.08, 8.2));
-		unitDB.DivineKnight.setName("DivineKnight");
+		unitDB.DivineKnight.setName("Divine Knight");
 		
 		// Create siege units
 		unitDB.setBricole(new unit(1, 1, 20, 9, 5, 7, 16.99, 10, 0.21, 1.0));
 		unitDB.Bricole.setName("Bricole");
 		unitDB.setAssaultCart(new unit(2, 4, 9, 14, 8, 5, 26.14, 1, 0.42, 1.4));
-		unitDB.AssaultCart.setName("AssaultCart");
+		unitDB.AssaultCart.setName("Assault Cart");
 		unitDB.setMangonel(new unit(3, 7, 38, 17, 7, 7, 18.30, 10, 0.63, 1.9));
 		unitDB.Mangonel.setName("Mangonel");
 		unitDB.setBatteringRam(new unit(4, 10, 17, 25, 13, 5, 27.45, 1, 0.83, 2.5));
-		unitDB.BatteringRam.setName("BatteringRam");
+		unitDB.BatteringRam.setName("Battering Ram");
 		unitDB.setHeavyMangonel(new unit(5, 13, 64, 28, 11, 7, 19.61, 10, 1.04, 3.2));
-		unitDB.HeavyMangonel.setName("HeavyMangonel");
+		unitDB.HeavyMangonel.setName("Heavy Mangonel");
 		unitDB.setDemolisher(new unit(6, 16, 28, 40, 19, 5, 28.75, 1, 1.25, 4.0));
 		unitDB.Demolisher.setName("Demolisher");
 		unitDB.setTrebuchet(new unit(7, 19, 98, 44, 16, 7, 20.91, 10, 1.46, 4.9));
 		unitDB.Trebuchet.setName("Trebuchet");
 		unitDB.setHeavyTrebuchet(new unit(8, 22, 118, 53, 19, 7, 20.91, 10, 1.67, 5.9));
-		unitDB.HeavyTrebuchet.setName("HeavyTrebuchet");
+		unitDB.HeavyTrebuchet.setName("Heavy Trebuchet");
 		unitDB.setSiegeTower(new unit(9, 26, 49, 70, 32, 5, 31.37, 1, 1.88, 7.0));
-		unitDB.SiegeTower.setName("SiegeTower");
+		unitDB.SiegeTower.setName("Siege Tower");
 		unitDB.setMightyTrebuchet(new unit(10, 30, 164, 73, 26, 7, 22.22, 10, 2.08, 8.2));
-		unitDB.MightyTrebuchet.setName("MightyTrebuchet");
+		unitDB.MightyTrebuchet.setName("Mighty Trebuchet");
 		
 		breakdownUnits(unitDB, masterScout, lordTroopBreakdown);
 		
@@ -401,8 +427,8 @@ public class UserInterface {
 		
 		lordTroopBreakdown.setText("The lord's estimated troop power is " + troopPow + "\n\n"
 				+ "The lord should have an estimated total of " + maxTroopCount + " troops at their disposal.\n\n"
-				+ one.count + " Level " + troopLevel + " " + one.getName()  + " \n" + two.count + " Level " + troopLevel +  " " + two.getName() + " \n"
-				+ three.count + " Level " + troopLevel +  " " + three.getName() + " \n" + four.count + " Level " + troopLevel + " " + four.getName() + " \n");
+				+ one.count + " Level " + troopLevel + " " + one.getName()  + " \n\n" + two.count + " Level " + troopLevel +  " " + two.getName() + " \n\n"
+				+ three.count + " Level " + troopLevel +  " " + three.getName() + " \n\n" + four.count + " Level " + troopLevel + " " + four.getName() + " \n\n");
 		
 	}
 }
